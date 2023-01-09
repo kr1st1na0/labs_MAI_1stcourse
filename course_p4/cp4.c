d44887abd3d76c8e6cd50f5c99a6cd9fa4b7e61d
kristina <kristina.bilckova@yandex.ru>
2023-01-10 02:11:11 +0300

    Labs

--- course_p4/cp4.c
+++ course_p4/cp4.c
@@ -3,9 +3,9 @@
 
 typedef double dbl;
 
-dbl epsilon(dbl x) {
+dbl epsilon() {
     dbl eps = 1.0;
-    while (x + eps / 2.0 != x)
+    while (1 + eps / 2.0 != 1)
         eps /= 2.0;
     return eps;
 }
@@ -50,11 +50,10 @@ dbl F3_second_derivative(dbl x) {
     return 1/((1 + x) * (1 + x)) - sin(x);
 }
 
-dbl dichotomy(dbl F(dbl), dbl a, dbl b) {
+dbl dichotomy(dbl (*F)(dbl), dbl a, dbl b, dbl relative_eps, dbl abs_eps) {
     dbl x = (a + b) / 2;
-    dbl eps = epsilon(x);
     if (F(a) * F(b) < 0){
-        while (fabs(a - b) > eps) {
+        while (fabs(a - b) > fmax(relative_eps * fabs(x), abs_eps)) {
             x = (a + b) / 2;
             if (F(x) * F(a) < 0) {
                 b = x;
@@ -70,11 +69,10 @@ dbl dichotomy(dbl F(dbl), dbl a, dbl b) {
     }
 }
 
-dbl iterations(dbl F_x(dbl), dbl F_x_first_derivative(dbl), dbl a, dbl b) {
+dbl iterations(dbl (*F_x)(dbl), dbl (*F_x_first_derivative)(dbl), dbl a, dbl b, dbl relative_eps, dbl abs_eps) {
     dbl x = (a + b) / 2;
-    dbl eps = epsilon(x);
     if (fabs(F_x_first_derivative(x)) < 1) {
-        while (fabs(F_x(x) - x) >= eps) {
+        while (fabs(F_x(x) - x) >= fmax(relative_eps * fabs(x), abs_eps)) {
             x = F_x(x);
         }
         return x;
@@ -84,11 +82,10 @@ dbl iterations(dbl F_x(dbl), dbl F_x_first_derivative(dbl), dbl a, dbl b) {
     }
 }
 
-dbl newton(dbl F(dbl), dbl F_first_derivative(dbl), dbl F_second_derivative(dbl), dbl a, dbl b) {
+dbl newton(dbl (*F)(dbl), dbl (*F_first_derivative)(dbl), dbl (*F_second_derivative)(dbl), dbl a, dbl b, dbl relative_eps, dbl abs_eps) {
     dbl x = (a + b / 2);
-    dbl eps = epsilon(x);
     if (fabs(F(x) * F_second_derivative(x)) < (F_first_derivative(x) * F_first_derivative(x))) {
-        while (fabs(F(x) / F_first_derivative(x)) > eps) {
+        while (fabs(F(x) / F_first_derivative(x)) > fmax(relative_eps * fabs(x), abs_eps)) {
             x -= F(x) / F_first_derivative(x);
         }
         return x;
@@ -109,17 +106,20 @@ void result(dbl d, dbl i, dbl n) {
 
 
 int main() {
+    dbl abs_eps = epsilon();
+    dbl relative_eps = sqrt(abs_eps);
     dbl a2 = 1, b2 = 2;
     dbl a3 = 1, b3 = 1.5;
-    dbl d1 = dichotomy(F2, a2, b2);
-    dbl i1 = iterations(F2_x, F2_x_first_derivative, a2, b2);
-    dbl n1 = newton(F2, F2_first_derivative, F2_second_derivative, a2, b2);
+    dbl d1 = dichotomy(F2, a2, b2, relative_eps, abs_eps);
+    dbl i1 = iterations(F2_x, F2_x_first_derivative, a2, b2, relative_eps, abs_eps);
+    dbl n1 = newton(F2, F2_first_derivative, F2_second_derivative, a2, b2, relative_eps, abs_eps);
+    printf("Machine epsilon for long double = %.16e\n", abs_eps);
     printf("Function cos(x) - exp(-0.5*x^2) + x - 1\n");
     result(d1, i1, n1);
     printf("\n");
-    dbl d2 = dichotomy(F3, a3, b3);
-    dbl i2 = iterations(F3_x, F3_x_first_derivative, a3, b3);
-    dbl n2 =newton(F3, F3_first_derivative, F3_second_derivative, a3, b3);
+    dbl d2 = dichotomy(F3, a3, b3, relative_eps, abs_eps);
+    dbl i2 = iterations(F3_x, F3_x_first_derivative, a3, b3, relative_eps, abs_eps);
+    dbl n2 =newton(F3, F3_first_derivative, F3_second_derivative, a3, b3, relative_eps, abs_eps);
     printf("Function 1 - x + sin(x) - ln(1 + x)\n");
     result(d2, i2, n2);
     return 0;
