d284dc2a84baa73fb3052a038e1a652c282ebccc
kristina <kristina.bilckova@yandex.ru>
2023-05-22 22:26:52 +0300

    gospody ya tak rada eto tak prekrasno

--- 2sem/course_p7/cp7.c
+++ 2sem/course_p7/cp7.c
@@ -3,152 +3,146 @@
 #include <stdbool.h>
 #include <string.h>
 #include <limits.h>
+#include <assert.h>
+#include <stdint.h>
+#include <errno.h>
+#include <stddef.h>
 
-typedef struct {
-    int *data;
-    int size;
-    int elemcount;
-} vector;
+#include "cp7.h"
 
-void vectorCreate(vector * v, int size) {
-    v->size = size;
-    v->elemcount = 0;
-    v->data = (int*)malloc(sizeof(int) * v->size);
+static int vectorCreate(Vector * const v, size_t initialCapacity) {
+    if (initialCapacity == 0)
+        v->data = NULL;
+    else {
+        v->data = (Elem *) malloc(sizeof(Elem) * initialCapacity);
+        if (v->data == NULL)
+            return errno;
+    }
+    v->capacity = initialCapacity;
+    v->size = 0;
+    return 0;
 }
 
-void vectorDestroy(vector* v) {
-    v->size = v->elemcount = 0;
+static void vectorClear(Vector * const v) {
+    v->size = 0;
+    // You can nullify data if you want, but it makes no difference
+}
+
+static void vectorDestroy(Vector * const v) {
+    v->capacity = v->size = 0;
     free(v->data);
 }
 
-void vectorPushBack(vector * v, int value) {
-    if (v->elemcount == v->size) {
-        v->size++;
-        v->data = realloc(v->data, sizeof(int) * v->size);
+static int vectorPushBack(Vector * const v, const Elem * const elem) {
+    if (v->size == v->capacity) {
+        v->capacity++;
+        v->data = realloc(v->data, sizeof(Elem) * v->capacity);
     }
-    v->data[v->elemcount++] = value;
+    v->data[v->size++] = *elem;
+    return 0;
 }
 
-int elemCount(char * name) {
-    FILE *f = fopen(name, "r");
-    int count = 0;
-    int elem = 0;
-    while (fscanf(f, "%d", &elem) && !feof(f)) {
-        count++; 
-    }
-    return count;
-}
-
-int linesCount(char * name) {
-    FILE *f = fopen(name, "r");
-    int count = 0;
-    while (!feof(f)) {
-        if (fgetc(f) == '\n') {
-            count++;
-        }
-    }
-    return count;
-}
-
-vector * matrixInput(char * name, vector * v) {
-    FILE *f = fopen(name, "r");
-    vectorCreate(v, 1);
-    int n = linesCount(name);
-    int m = elemCount(name) / n;
-    int *arr;
-    arr = (int*)malloc(sizeof(int) * n * m);
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < m; j++) {
-            fscanf(f, "%d", &arr[i * m + j]);
-        }
-    }
-    // PushBack
-    for (int i = 0; i < n; i++) {
-        int flag = 0;
-        for (int j = 0; j < m; j++) {
-            if (arr[i * m + j] != 0) {
-                if (flag == 0) { // line
-                    vectorPushBack(v, i + 1);
-                    ++flag;
-                }
-                vectorPushBack(v, j + 1); // column
-                vectorPushBack(v, arr[i * m + j]); // elem
-            }
-        }
-        if (flag != 0) {
-            vectorPushBack(v, 0); // ending of the line
-        }
-    }
-    vectorPushBack(v, 0); // ending of the matrix
-    free(arr);
-    return v;
-}
-
-int vectorMax(vector * v) {
+static int vectorMax(Vector * v) {
     int max = INT_MIN;
-    for (int i = 2; i < v->size - 2; i += 2) {
-        if (v->data[i] > max) {
-            max = v->data[i];
+    for (int i = 1; i < v->capacity; ++i) {
+        if (v->data[i].value > max) {
+            max = v->data[i].value;
         }
     }
     return max;
 }
 
-void vectorTask(char * name, vector * v) {
-    int max = vectorMax(v);
-    int column = 0;
-    for (int i = 2; i < v->size - 2; i += 2) {
-        if (v->data[i] == max) {
-            column = v->data[i - 1];
-            break;
+static void vectorPrint(Vector * v) {
+    if (v == NULL)
+        return;
+    printf("Matrix in one vector:\n");
+    for (int i = 0; i < v->capacity; ++i) {
+        printf("%ld ", v->data[i].j);
+        printf("%d ", v->data[i].value);
+    }
+    printf("\n");
+}
+
+int matrixCreate(Matrix * const matrix) {
+    const int err = vectorCreate(&matrix->vector, 1);
+    if (err != 0)
+        return err;
+    matrix->vector.data->j = matrix->vector.data->i = -1;
+    matrix->m = matrix->n = 0;
+    return 0;
+}
+
+void matrixDestroy(Matrix *matrix) {
+    vectorDestroy(&matrix->vector);
+    matrix->m = matrix->n = 0;
+}
+
+int matrixInput(FILE * f, Matrix * matrix) {
+    size_t m, n;
+    if (fscanf(f, "%zu%zu", &m, &n) != 2)
+        return errno;
+    vectorClear(&matrix->vector);
+    matrix->m = m;
+    matrix->n = n;
+    for (size_t i = 0; i < m; ++i) {
+        bool flag = false;
+        for (size_t j = 0; j < n; ++j) {
+            int value;
+            if (fscanf(f, "%d", &value) != 1)
+                return errno;
+            if (value == 0)
+                continue;
+            if (!flag && vectorPushBack(&matrix->vector, &(Elem) { .j = -1, .i = i }) != 0)
+                return errno;
+            if (vectorPushBack(&matrix->vector, &(Elem) { .j = j, .value = value }) != 0)
+                return errno;
+            flag = true;
         }
-        if (v->data[i + 1] == 0) {
-            i += 2;
-            continue;
+    }
+    vectorPushBack(&matrix->vector, &(Elem) { .j = -1, .i = -1 }); // ending of the matrix
+    return 0;
+}
+
+void matrixTask(Matrix * matrix) {
+    int max = vectorMax(&matrix->vector);
+    int column = 0;
+    for (int i = 0; i < matrix->vector.capacity; ++i) {
+        if (matrix->vector.data[i].value == max) {
+            column = matrix->vector.data[i].j;
+            break;
         }
     }
     printf("Max = %d, Column = %d\n", max, column);
     printf("Task:\n");
-    FILE *f = fopen(name, "r");
-    int n = linesCount(name); // lines
-    int m = elemCount(name) / n; //columns
-    fclose(f);
-    vector temp;
-    vectorCreate(&temp, n * m); // не знаю как, но оно заполнилось, ну и слава богу
-    // (заполнило всё идеально, кроме первой строки матрицы)
-    // for (int i = 0; i < m * n; i++) {
-    //     printf("%d ", temp.data[i]);
-    // }
-    int k = 2;
-    for  (int i = 0; i < m; i++) {
-        if (v->data[k - 1] == i + 1) {
-            temp.data[i] = v->data[k];
-            k += 2;
-        }
+    int p, q;
+    int n = matrix->n;
+    int m = matrix->m;
+    int **temp;
+    temp = (int**)malloc(n * sizeof(int*));
+    for (int i = 0; i < m; ++i) {
+        temp[i] = (int*)malloc(m * sizeof(int));
+    }        
+    for (int i = 0; i < matrix->vector.capacity - 1; ++i) {
+        if (matrix->vector.data[i].j == -1) {
+            p = matrix->vector.data[i].i;
+        } 
+        if (matrix->vector.data[i].j != -1) {
+            q = matrix->vector.data[i].j;
+            temp[p][q] = matrix->vector.data[i].value;
+        } 
     }
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < m; j++) {
-            if (j == column - 1) {
-                double tmp = temp.data[i * m + j];
+    for (int i = 0; i < m; ++i) {
+        for  (int j = 0; j < n; ++j) {
+            if (j == column) {
+                double tmp = temp[i][j];
                 printf("%.2f ", tmp / max);
             }
             else {
-                printf("%d ", temp.data[i * m + j]);
+                printf("%d ", temp[i][j]);
             }
         }
         printf("\n");
-    } 
-}
-
-void vectorPrint(FILE * f, vector * v) {
-    if (v == NULL) { 
-        return;
     }
-    printf("Matrix in one vector:\n");
-    for (int i = 0; i < v->size; i++) {
-        printf("%d ", v->data[i]);
-    }
-    printf("\n");
 }
 
 int main(int argc, char * argv[]) {
@@ -158,32 +152,11 @@ int main(int argc, char * argv[]) {
         printf("Can not open file\n");
         return 0;
     }
-    vector vec;
-    vector *v = &vec;
-    v = matrixInput(name, v);
-    vectorPrint(f, v);
-    vectorTask(name, v);
+    Matrix * matrix = malloc(sizeof(Matrix));
+    matrixCreate(matrix);
+    matrixInput(f, matrix);
+    vectorPrint(&matrix->vector);
+    matrixTask(matrix);
     fclose(f);
     return 0;
 }
-
-
-/*
-Можно просто пихнуть в "void vectorTask(...)"
-// Вывод задания в виде вектора (без нулевых элементов)
-    printf("Task in vector:\n");
-    for (int i = 2; i < v->size - 2; i += 2) {
-        if (v->data[i - 1] == column) {
-            double temp = v->data[i];
-            printf("%.2f ", temp / max);
-        }
-        else {
-            printf("%d ", v->data[i]);
-        }
-        if (v->data[i + 1] == 0) {
-            i += 2;
-            continue;
-        }
-    }
-    printf("\n");
-*/
---
+++ 2sem/course_p7/cp7.h
@@ -0,0 +1,36 @@
+#ifndef CP7_H
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+#include <limits.h>
+#include <assert.h>
+#include <stdint.h>
+#include <errno.h>
+#include <stddef.h>
+
+typedef struct {
+    ptrdiff_t j;
+    union {
+        int value;
+        ptrdiff_t i;
+    };
+} Elem;
+
+typedef struct {
+    Elem *data;
+    size_t capacity, size;
+} Vector;
+
+typedef struct {
+    Vector vector;
+    size_t m, n;
+} Matrix;
+
+int matrixCreate(Matrix *matrix);
+
+void matrixDestroy(Matrix *matrix);
+
+int matrixInput(FILE * f, Matrix * matrix);
+
+#endif
